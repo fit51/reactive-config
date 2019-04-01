99bbea1d66e6c3f96a4223981c7e91fe302890a8
p.kondratyuk <p.kondratyuk@tinkoff.ru>
2019-04-01 15:38:27 +0300

    Add examples module with SampleApplication

--- build.sbt
+++ build.sbt
@@ -2,10 +2,10 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.12.8",
   libraryDependencies ++= Seq(
     "io.monix"                   %% "monix"          % "3.0.0-RC2",
-    "org.mockito"                % "mockito-core"    % "2.7.19" % Test,
-    "org.scalatest"              %% "scalatest"      % "3.0.4" % Test,
     "org.slf4j"                  % "slf4j-api"       % "1.7.7",
     "com.typesafe.scala-logging" %% "scala-logging"  % "3.7.2",
+    "org.mockito"                % "mockito-core"    % "2.7.19" % Test,
+    "org.scalatest"              %% "scalatest"      % "3.0.4" % Test,
     "ch.qos.logback"             % "logback-classic" % "1.2.3" % Test,
     "ch.qos.logback"             % "logback-core"    % "1.2.3" % Test
   ),
@@ -45,6 +45,19 @@ lazy val typesafe = project
       "com.github.pathikrit" %% "better-files"  % "3.7.0",
       "io.circe"             %% "circe-parser"  % "0.10.0-M1" % Test,
       "io.circe"             %% "circe-generic" % "0.9.3" % Test
+    )
+  )
+
+lazy val examples = project
+  .in(file("examples"))
+  .dependsOn(core, circe, typesafe)
+  .settings(commonSettings)
+  .settings(
+    name := "reactive-config-examples",
+    libraryDependencies ++= Seq(
+      "io.circe"       %% "circe-generic"  % "0.9.3",
+      "ch.qos.logback" % "logback-classic" % "1.2.3",
+      "ch.qos.logback" % "logback-core"    % "1.2.3"
     )
   )
 
---
+++ examples/src/main/scala/com/github/fit51/reactiveconfig/examples/SampleApplication.scala
@@ -0,0 +1,55 @@
+package com.github.fit51.reactiveconfig.examples
+
+import java.nio.file.Paths
+
+import monix.eval.Task
+import com.typesafe.scalalogging.LazyLogging
+import io.circe._
+import io.circe.generic.auto._
+import monix.execution.Cancelable
+import com.github.fit51.reactiveconfig.config._
+import com.github.fit51.reactiveconfig.parser._
+import com.github.fit51.reactiveconfig.typesafe.TypesafeConfigStorage
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+case class SimpleLib(foo: String, whatever: String)
+
+object SampleApplication extends App with LazyLogging {
+  implicit val scheduler = monix.execution.Scheduler.global
+
+  import CirceConfigParser._
+  import CirceConfigDecoder._
+
+  def useConfig(config: ReactiveConfig[Task, Json]): Task[Cancelable] = {
+    val some      = config.reloadable[String]("complex-app.something")
+    val libConfig = config.reloadable[SimpleLib]("complex-app.simple-lib-context.simple-lib")
+    val combined  = libConfig.combine(some)((l, s) => l.copy(foo = s))
+
+    Task.eval {
+      println("Entered")
+      scheduler.scheduleWithFixedDelay(0 seconds, 1 second) {
+        println(s"some: ${some.get}")
+        println(s"lib: ${libConfig.get}")
+        println(s"all together: ${combined.get}")
+      }
+    }
+  }
+
+  val app = for {
+    storage     <- Task.eval(TypesafeConfigStorage[Task, Json](Paths.get("examples/config/application.conf")))
+    config      <- ReactiveConfigImpl[Task, Json](storage)
+    _           <- Task.eval(println("Now change examples/config/application.conf file and see what happens!"))
+    cancellable <- useConfig(config)
+    _           <- Task.sleep(1.minute)
+    _           <- Task.eval(cancellable.cancel())
+  } yield ()
+
+  Await.result(
+    app.doOnFinish {
+      case Some(e) => Task.eval(println("Error", e))
+      case None    => Task.eval(println("Finished"))
+    }.runToFuture,
+    Duration.Inf
+  )
+}
