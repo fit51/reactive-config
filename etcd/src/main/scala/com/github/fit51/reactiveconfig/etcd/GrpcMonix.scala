7a13a5e997354e49dce538279000c700cec9ba97
p.kondratyuk <p.kondratyuk@tinkoff.ru>
2020-03-10 19:55:58 +0300

    Fix GrpcMonix convertion

--- etcd/src/main/scala/com/github/fit51/reactiveconfig/etcd/GrpcMonix.scala
+++ etcd/src/main/scala/com/github/fit51/reactiveconfig/etcd/GrpcMonix.scala
@@ -4,11 +4,13 @@ import io.grpc.stub.StreamObserver
 import monix.reactive.observers.Subscriber
 
 object GrpcMonix {
-  def monixSubscriberToGrpcObserver[T](subscriber: Subscriber[T]): StreamObserver[T] =
+  def monixToGrpcObserver[T](subscriber: Subscriber[T]): StreamObserver[T] = {
+    //Using system default batch size (1024) of BatchedExecution model
+    val rSubscriber = Subscriber.toReactiveSubscriber(subscriber)
     new StreamObserver[T] {
-      override def onError(t: Throwable): Unit = subscriber.onError(t)
-      override def onCompleted(): Unit         = subscriber.onComplete()
-      // No backpressure here, breaking the observer contract
-      override def onNext(value: T): Unit = subscriber.onNext(value)
+      override def onNext(value: T): Unit      = rSubscriber.onNext(value)
+      override def onError(t: Throwable): Unit = rSubscriber.onError(t)
+      override def onCompleted(): Unit         = rSubscriber.onComplete()
     }
+  }
 }
