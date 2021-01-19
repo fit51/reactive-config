10331b15e5a86b7bf408b1d80d378ca1f85f3faa
realvikr <vs@vikr.su>
2021-01-19 09:12:50 +0500

    Delete useless CircuitBreaker in EtcdWatch

--- etcd/src/main/scala/com/github/fit51/reactiveconfig/etcd/EtcdWatch.scala
+++ etcd/src/main/scala/com/github/fit51/reactiveconfig/etcd/EtcdWatch.scala
@@ -8,10 +8,8 @@ import com.github.fit51.reactiveconfig.etcd.gen.kv.KeyValue
 import com.github.fit51.reactiveconfig.etcd.gen.rpc.WatchRequest.RequestUnion.CreateRequest
 import com.github.fit51.reactiveconfig.etcd.gen.rpc.{WatchCreateRequest, WatchGrpc, WatchRequest, WatchResponse}
 import io.grpc.stub.StreamObserver
-import monix.catnap.CircuitBreaker
 import monix.eval.{Task, TaskLift}
 import monix.execution.Ack.{Continue, Stop}
-import monix.execution.exceptions.ExecutionRejectedException
 import monix.execution.{Ack, Scheduler}
 import monix.reactive.Observable
 import monix.reactive.observers.Subscriber
@@ -30,23 +28,6 @@ trait Watch[F[_]] {
   def onErrorDelay: FiniteDuration
 
   implicit def taskLift: TaskLift[F]
-
-  /**
-    * This CB protects subscribe method.
-    */
-  private val circuitBreaker = CircuitBreaker[Task].unsafe(
-    maxFailures = 2,
-    resetTimeout = 4.seconds,
-    onOpen = Task {
-      logger.error("ETCD: Watch is unavailiable!")
-    },
-    onHalfOpen = Task {
-      logger.warn("ETCD: Watch is trying to connect!")
-    },
-    onClosed = Task {
-      logger.warn("ETCD: Watch connected to Etcd!")
-    }
-  )
 
   private lazy val watchService = WatchGrpc.stub(manager.channel)
 
@@ -111,18 +92,13 @@ trait Watch[F[_]] {
       keyRange: KeyRange,
       p: Deferred[Task, Unit]
   ): Task[Unit] =
-    circuitBreaker.protect {
-      for {
-        map <- watchIds.read
-        id = map.get(keyRange.start)
-        _ <- Task.raiseError(new Exception("Watch already exists")).whenA(id.nonEmpty)
-        _ = subscribe(subscriber, keyRange, p)
-        _ <- p.get
-      } yield ()
-    }.onErrorRecoverWith {
-      case _: ExecutionRejectedException =>
-        circuitBreaker.awaitClose >> protectedSubscribe(subscriber, keyRange, p)
-    }
+    for {
+      map <- watchIds.read
+      id = map.get(keyRange.start)
+      _ <- Task.raiseError(new Exception("Watch already exists")).whenA(id.nonEmpty)
+      _ = subscribe(subscriber, keyRange, p)
+      _ <- p.get
+    } yield ()
 
   /** Subscribes on Watch Events for defined keyRange
     */
